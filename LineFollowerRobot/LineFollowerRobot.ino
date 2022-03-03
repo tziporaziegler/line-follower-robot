24d720006d078be692437aa745e4532dd1ab930b
Tzipora Ziegler <tziporaziegler@users.noreply.github.com>
2022-03-03 17:29:57 -0500

    Add more logic

--- LineFollowerRobot/LineFollowerRobot.ino
+++ LineFollowerRobot/LineFollowerRobot.ino
@@ -6,12 +6,12 @@
 #include "EMIC2.h"
 
 // Ultrasonic distance sensor - eyes
-#define TRIGGER_PIN            5
-#define ECHO_PIN               6
+#define TRIGGER_PIN            7
+#define ECHO_PIN               8
 #define MAX_DISTANCE           70 // Max distance to ping for (in cm). Maximum distance is rated at 400-500cm.
-                                  // Target objects will be 30 - 50 cm away.
+// Target objects will be 30 - 50 cm away.
 #define MIN_DISTANCE           6  // Min distance allowed between the robot and an object.
-                                  // Robot required to stop 5 - 8 cm before objects.
+// Robot required to stop 5 - 8 cm before objects.
 
 // Servo - head
 #define SERVO_PIN              9
@@ -20,21 +20,32 @@
 #define SERVO_MIDDLE_POSITION  (SERVO_MAX_POSITION - SERVO_MIN_POSITION) / 2
 
 // Emic 2
-#define RX_PIN                 10  // Connect to SOUT pin
-#define TX_PIN                 11  // Connect to SIN pin
-#define EMIC_VOLUME            -10 // -48 to 18 dB
+#define RX_PIN                 6  // Connect to SOUT pin
+#define TX_PIN                 5  // Connect to SIN pin
+#define EMIC_VOLUME            0 // -48 to 18 dB
 #define EMIC_VOICE             6   // 9 choices: 0-8
+
+#define RIGHT_WHEEL_PIN        10
+#define LEFT_WHEEL_PIN         11
 
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 Servo servo;
 EMIC2 emic;
 
+Servo rightWheel;
+Servo leftWheel;
+
 bool checkForIntersections; // Use to make the drive home faster
 int numPackagesDelivered;
+
+int intersectionNum = 0;
 
 void setup() {
   Serial.begin(115200); // Open serial monitor at 115200 baud instead of 9600 to see ping results.
   servo.attach(SERVO_PIN);
+
+  rightWheel.attach(RIGHT_WHEEL_PIN);
+  leftWheel.attach(LEFT_WHEEL_PIN);
 
   turnHeadToFaceForward();
 
@@ -44,7 +55,8 @@ void setup() {
   emic.setVolume(EMIC_VOLUME);
   emic.setVoice(EMIC_VOICE);
 
-  talk("Hello, there! I have some packages to deliver. Let's go!");
+//  talk("Hello, there! I have some packages to deliver. Let's go!");
+talk("Starting");
 
   checkForIntersections = true;
   numPackagesDelivered = 0;
@@ -55,40 +67,46 @@ void loop() {
 
   if (checkForIntersections && intersectionDetected()) {
     talk("Intersection detected");
+    intersectionNum++;
 
-    const bool objectOnRight = checkForObjectOnRight();
-    const bool objectOnLeft = checkForObjectOnLeft();
-    turnHeadToFaceForward();
+    if (intersectionNum == 1) {
+      // Figure out which way to continue
+    } else if (intersectionNum >= 3) {
+      const bool objectOnRight = checkForObjectOnRight();
+      const bool objectOnLeft = checkForObjectOnLeft();
+      turnHeadToFaceForward();
 
-    if (objectOnRight) {
-      turnRight();
-      // TODO: Drive until distance sensor detects that short enough distance from the object
-      deliverPackage();
-      reverseDirection();
-      // TODO: Drive same distance back to main road
-      turnRight();
+      if (objectOnRight) {
+        turnRight();
+        // TODO: Drive until distance sensor detects that short enough distance from the object
+        deliverPackage();
+        reverseDirection();
+        // TODO: Drive same distance back to main road
+        turnRight();
+      }
+
+      if (objectOnLeft) {
+        turnLeft();
+        // TODO: Drive until distance sensor detects that short enough distance from the object
+        deliverPackage();
+        reverseDirection();
+        // TODO: Drive same distance back to main road
+        turnLeft();
+      }
+
+      // TODO: Drive enough distance to ensure doesn't detect intersection again
     }
 
-    if (objectOnLeft) {
-      turnLeft();
-      // TODO: Drive until distance sensor detects that short enough distance from the object
-      deliverPackage();
-      reverseDirection();
-      // TODO: Drive same distance back to main road
-      turnLeft();
+    if (endDetected()) {
+      celebrate();
+      goHome();
     }
-
-    // TODO: Drive enough distance to ensure doesn't detect intersection again
-  }
-
-  if (endDetected()) {
-    celebrate();
-    goHome();
   }
 }
 
 void driveForward() {
-  // TODO: Move wheels forward x amount using IR sensors to follow the line
+  rightWheel.write(180);
+  leftWheel.write(180);
 }
 
 void turnRight() {
@@ -104,7 +122,6 @@ void reverseDirection() {
 }
 
 bool intersectionDetected() {
-  // TODO: Return true if all 3 IR sensors detect black
   return false;
 }
 
